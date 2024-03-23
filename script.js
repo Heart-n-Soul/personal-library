ecdacf6d4da96c04e912daa8deadd39d3f3a998b
Emmanuel Banahene Asante <emmaasante676@gmail.com>
2024-03-23 17:10:31 +0000

    add book constructor and library array

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const myLibrary = [];
+
+
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    
+}
+
+const obj = new Book("Kwame", "c.k mann", 23, true)
+
+function addBookToLibrary() {
+    
+}
+
+console.log(obj);
+
